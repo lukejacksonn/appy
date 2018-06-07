8578d9c8ed7d38d6b1a4bced8101b82636c43986
Luke Jackson <lukejacksonn@gmail.com>
2018-06-07 10:19:19 +0100

    Seconds commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ class App extends Component {
 
   state = {
     name: 'Luke',
+    temp: 0,
   }
 
   fetchWeather = city => fetch(
@@ -12,8 +13,10 @@ class App extends Component {
   )
     .then(response => response.json())
     .then(json => {
-      this.setState({ temp: json.main.temp })
+      this.setState({ name: json.name, temp: json.main.temp })
     })
+
+  kelvinToC = kelvin => Math.round(kelvin - 273.15)
 
   componentDidMount() {
     this.fetchWeather('london')
@@ -23,15 +26,21 @@ class App extends Component {
     console.log(this.state)
     return (
       <main>
-        <select onChange={e => this.fetchWeather(e.target.value)}>
-          <option value='london'>London</option>
-          <option value='newcastle'>Newcastle</option>
-          <option value='dubai'>Dubai</option>
+        <h1>{this.state.temp} kelvin</h1>
+        <h1>{this.kelvinToC(this.state.temp)} celsius</h1>
+        <select onChange={event => this.fetchWeather(event.target.value)}>
+          <option>London</option>
+          <option>Newcastle</option>
         </select>
-        <h1>{this.state.temp}</h1>
       </main>
     )
   }
 }
+
+// <select onChange={e => this.fetchWeather(e.target.value)}>
+//   <option value='london'>London</option>
+//   <option value='newcastle'>Newcastle</option>
+//   <option value='dubai'>Dubai</option>
+// </select>
 
 export default App
